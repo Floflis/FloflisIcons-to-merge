f1516cc2cd3204f0ee833e6ea43e9cbeb085b7c9
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-22 17:03:31 -0300

    complement commits 5f774fce3e752d2247135995513a34ff619e43f3 and 81c9e0276ff5f9e36d1bace899e43119b596da5d

--- install-2.sh
+++
@@ -1,4 +0,0 @@
-echo "- Copying (probably) a lot of files. Don't worry if it takes several times..."
-sudo mkdir /usr/share/icons/Floflis-to-merge
-sudo cp -r -f --preserve=all . /usr/share/icons/Floflis-to-merge
-sudo rm -r /usr/share/icons/Floflis-to-merge/.git
