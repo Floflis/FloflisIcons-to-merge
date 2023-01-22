81c9e0276ff5f9e36d1bace899e43119b596da5d
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-22 13:57:18 -0300

    install.sh: dont attempt to create folders that already exist

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "- Copying (probably) a lot of files. Don't worry if it takes several times..."
-sudo mkdir /usr/share/icons/Floflis-to-merge
+if [ ! -e /usr/share/icons/Floflis-to-merge ]; then sudo mkdir /usr/share/icons/Floflis-to-merge; fi
+ln -s Floflis-to-merge /usr/share/icons/Floflis
 sudo cp -r -f --preserve=all . /usr/share/icons/Floflis-to-merge
 sudo rm /usr/share/icons/Floflis-to-merge/.git
